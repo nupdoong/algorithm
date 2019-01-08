c78df5e8731adaace7934954dd1e5384acd25545
nupdoong <37859149+nupdoong@users.noreply.github.com>
2019-01-08 21:12:32 +0900

    Create Lv1_완주하지못한선수.cpp

---
+++ programmers/Lv1_완주하지못한선수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    const int size = participant.size();
+    
+    for(int i=0; i<size; i++) {
+ 
+        if(participant[i] != completion[i]) {
+            answer = participant[i];
+            break;
+        }
+    }
+    return answer;
+}
