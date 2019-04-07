17ea132be4e3308577e35c3db5992f987a5d3639
nupdoong <37859149+nupdoong@users.noreply.github.com>
2019-04-07 18:06:30 +0900

    Update 17136.cpp

--- BAEKJOON/17136.cpp
+++ BAEKJOON/17136.cpp
@@ -17,8 +17,9 @@ int check(int x, int y, int num){
     }
     return 0;
 }
-void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
+void dfs(int A1, int A2, int A3, int A4, int A5, int sum){
     int Check = 0;
+    int x, y;
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
             if(map[i][j]==1 && mask[i][j] == 0){
@@ -39,7 +40,7 @@ void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
     }
     if(check(x, y, 1) && A1 > 0){
         mask[x][y] = 1;
-        dfs(x, y, A1-1, A2, A3, A4, A5, sum+1);
+        dfs(A1-1, A2, A3, A4, A5, sum+1);
         mask[x][y] = 0;
     }
     if(check(x, y, 2) && A2 > 0){
@@ -48,7 +49,7 @@ void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
                 mask[i][j] = 1;
             }
         }
-        dfs(x, y, A1, A2-1, A3, A4, A5, sum+1);
+        dfs(A1, A2-1, A3, A4, A5, sum+1);
         for(int i=x; i<x+2; i++){
             for(int j=y; j<y+2; j++){
                 mask[i][j] = 0;
@@ -61,7 +62,7 @@ void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
                 mask[i][j] = 1;
             }
         }
-        dfs(x, y, A1, A2, A3-1, A4, A5, sum+1);
+        dfs(A1, A2, A3-1, A4, A5, sum+1);
         for(int i=x; i<x+3; i++){
             for(int j=y; j<y+3; j++){
                 mask[i][j] = 0;
@@ -74,7 +75,7 @@ void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
                 mask[i][j] = 1;
             }
         }
-        dfs(x, y, A1, A2, A3, A4-1, A5, sum+1);
+        dfs(A1, A2, A3, A4-1, A5, sum+1);
         for(int i=x; i<x+4; i++){
             for(int j=y; j<y+4; j++){
                 mask[i][j] = 0;
@@ -87,7 +88,7 @@ void dfs(int x, int y, int A1, int A2, int A3, int A4, int A5, int sum){
                 mask[i][j] = 1;
             }
         }
-        dfs(x, y, A1, A2, A3, A4, A5-1, sum+1);
+        dfs(A1, A2, A3, A4, A5-1, sum+1);
         for(int i=x; i<x+5; i++){
             for(int j=y; j<y+5; j++){
                 mask[i][j] = 0;
@@ -105,7 +106,7 @@ int main(void){
             mask[i][j] = 0;
         }
     }
-    dfs(0, 0, 5, 5, 5, 5, 5, 0);
+    dfs(5, 5, 5, 5, 5, 0);
     if(Max > 25){
         if(ans == 0)
             printf("0");
