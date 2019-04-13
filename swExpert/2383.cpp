1748a3ccba95c78b37d16df9b15216c671caaabd
nupdoong <37859149+nupdoong@users.noreply.github.com>
2019-04-13 22:33:18 +0900

    Update 2383.cpp

--- swExpert/2383.cpp
+++ swExpert/2383.cpp
@@ -1,181 +1,140 @@
 #include <iostream>
-#include <queue>
+#include <math.h>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
 using namespace std;
-
 int N;
 int map[10][10];
 int p[10][2];
+int p_num;
+int Min;
+int ans = 2e9;
+int s_l[2];
 int s[2][2];
-int sx1, sy1;
-int sx2, sy2;
-int p_count;
-int s_count;
-int Min = 1e9;
-vector<int> v1;
-vector<int> v2;
-
-int solve(){
-	int Count = 0;
-	int v1_count = 0;
-	int v2_count = 0;
-    queue<int> q1;
-	queue<int> q2;
-    vector<int> v3 = v1;
-    vector<int> v4 = v2;
-	sort(v3.begin(), v3.end());
-	sort(v4.begin(), v4.end());
-	while(1){
-		while(!q1.empty()){
-			int t = q1.front();
-			if(t + map[sx1][sy1] <= Count){
-				q1.pop();
-				continue;
-			}
-			else{
-				break;
-			}
-		}
-        while(1){
-            if(v1_count < v3.size()){
-				if(Count >= v3[v1_count]){
-					if(q1.size() < 3){
-                    	if(Count == v3[v1_count]){
-							q1.push(Count+1);
-							v1_count++;
-                    	}
-                    	else{
-                        	q1.push(Count);
-                        	v1_count++;
-                    	}
-					}
-                    else
-                        break;
-				}
-                else
-                    break;
+int s_num;
+vector<pair<int, pair<int, int>>> v1;
+vector<pair<int, pair<int, int>>> v2;
+queue<pair<int, pair<int, int>>> q1;
+queue<pair<int, pair<int, int>>> q2;
+void solve(){
+    int t=0;
+    int Count = 0;
+    vector<pair<int, int>> v3[10];
+	vector<pair<int, int>> v4[10];
+    while(1){
+        while(!q1.empty()){
+            int st = q1.front().first;
+            if(st+s_l[0]<=t){
+                q1.pop();
             }
-            else
+            else{
                 break;
-		}
-		while(!q2.empty()){
-			int t2 = q2.front();
-			if(t2 + map[sx2][sy2] <= Count){
-				q2.pop();
-				continue;
-			}
-			else{
-				break;
-			}
-		}
-        while(1){
-            if(v2_count < v4.size()){
-				if(Count >= v4[v2_count]){
-					if(q2.size() < 3){
-                    	if(Count == v4[v2_count]){
-							q2.push(Count+1);
-							v2_count++;
-                    	}
-                    	else{
-                        	q2.push(Count);
-                        	v2_count++;
-                    	}
-					}
-                    else
-                        break;
-				}
-                else
-                    break;
             }
-            else
+        }
+        for(int i=0; i<v1.size();i++){
+            int x = v1[i].second.first;
+            int y = v1[i].second.second;
+            if(v1[i].first==t-1){
+                if(q1.size()<3){
+                    Count++;
+                    q1.push(make_pair(t, make_pair(x, y)));
+                }
+                else{
+                    v3[i].push_back(make_pair(i, v1[i].first));
+                    v1[i].first++;
+                }
+            }
+        }
+        while(!q2.empty()){
+            int st = q2.front().first;
+            if(st+s_l[1]<=t){
+                q2.pop();
+            }
+            else{
                 break;
-		}
-		if(v1_count == v3.size() && v2_count == v4.size() && q1.empty() == 1 && q2.empty() == 1){
-			break;
-		}
-		Count++;
-	}
-	return Count;
+            }
+        }
+        for(int i=0; i<v2.size();i++){
+            int x = v2[i].second.first;
+            int y = v2[i].second.second;
+            if(v2[i].first==t-1){
+                if(q2.size()<3){
+                    Count++;
+                    q2.push(make_pair(t, make_pair(x, y)));
+                }
+                else{
+                    v4[i].push_back(make_pair(i, v2[i].first));
+                    v2[i].first++;
+                }
+            }
+        }
+        
+        if(Count==p_num && q1.empty()==1 && q2.empty()==1){
+            Min = t;
+            break;
+        }
+        t++;
+    }
+    for(int i=0; i<p_num; i++){
+        if(v3[i].size()>0){
+        	int xx = v3[i][0].first;
+        	int yy = v3[i][0].second;
+        	v1[xx].first = yy;
+        }
+    }
+    for(int i=0; i<p_num; i++){
+        if(v4[i].size()>0){
+        	int xx = v4[i][0].first;
+        	int yy = v4[i][0].second;
+        	v2[xx].first = yy;
+        }
+    }
 }
-
-void dfs(int p_num){
-	if(p_num == p_count){
-		int tt = solve();
-		if(Min > tt){
-			Min = tt;
-		}
-		return;
-	}
-	int sum = 0;
-	int x = p[p_num][0];
-	int y = p[p_num][1];
-	
-	if(sx1- x < 0){
-		sum += x - sx1;
-	}
-	else{
-		sum += sx1 -x;
-	}
-	if(sy1 - y < 0){
-		sum += y - sy1;
-	}
-	else{
-		sum += sy1 - y;
-	}
-	v1.push_back(sum);
-	dfs(p_num+1);
-	v1.pop_back();
-	sum = 0;
-	if(sx2- x < 0){
-		sum += x - sx2;
-	}
-	else{
-		sum += sx2 -x;
-	}
-	if(sy2 - y < 0){
-		sum += y - sy2;
-	}
-	else{
-		sum += sy2 - y;
-	}
-	v2.push_back(sum);
-	dfs(p_num+1);
-	v2.pop_back();
+void dfs(int num){
+    if(num==p_num){
+        solve();
+        if(ans > Min){
+            ans = Min;
+        }
+        return;
+    }
+    int x = p[num][0];
+    int y = p[num][1];
+    int t = abs(x-s[0][0]) + abs(y-s[0][1]);
+    v1.push_back(make_pair(t, make_pair(x, y)));
+    dfs(num+1);
+    v1.pop_back();
+    t = abs(x-s[1][0]) + abs(y-s[1][1]);
+    v2.push_back(make_pair(t, make_pair(x, y)));
+    dfs(num+1);
+    v2.pop_back();
 }
-
 int main(void){
-	int T, test_case;
-	scanf("%d", &test_case);
-	for(T=1; T<=test_case; T++){
-        Min = 1e9;
-        v1.clear();
-        v2.clear();
-		p_count = 0;
-		s_count = 0;
-		scanf("%d", &N);
-		for(int i=0; i<N; i++){
-			for(int j=0; j<N; j++){
-				scanf("%d", &map[i][j]);
-				if(map[i][j] == 1){
-					p[p_count][0] = i;
-					p[p_count][1] = j;
-					p_count++;
-				}
-				if(map[i][j] > 1){
-					s[s_count][0] = i;
-					s[s_count][1] = j;
-					s_count++;
-				}
-			}
-		}
-		sx1 = s[0][0];
-		sy1 = s[0][1];
-		sx2 = s[1][0];
-		sy2 = s[1][1];
-		dfs(0);
-        printf("#%d %d\n", T, Min);
-	}
-	return 0;
+    int tc;
+    scanf("%d", &tc);
+    for(int T=1; T<=tc; T++){
+        ans = 2e9;
+        p_num=0;
+        s_num=0;
+        scanf("%d", &N);
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                scanf("%d", &map[i][j]);
+                if(map[i][j]==1){
+                    p[p_num][0] = i;
+                    p[p_num][1] = j;
+                    p_num++;
+                }
+                if(map[i][j]>1){
+                    s[s_num][0] = i;
+                    s[s_num][1] = j;
+                    s_l[s_num] = map[i][j];
+                    s_num++;
+                }
+            }
+        }
+        dfs(0);
+        printf("#%d %d\n", T, ans);
+    }
+    return 0;
 }
